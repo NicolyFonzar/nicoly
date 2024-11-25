179c5201799e8c8110fed6f4bbfcd3aa012f9a29
NicolyFonzar <Nicolyfonzar08@hotmail.com>
2024-11-25 19:48:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Qual é o maior planeta do sistema Solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Geral',
+    'Quem escreveu a obra Dom quixote?',
+    'Miguel de cervantes'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o elemento químico com o símbolo O?',
+    'Oxigênio'
+)
